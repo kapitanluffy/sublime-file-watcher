aac680fa3caad7455c938a5206dd2bedd07dfd08
kapitanluffy <kapitanluffy@gmail.com>
2024-02-14 11:36:28 +0800

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,4 +1,16 @@
-from .src import *
+import sys
+
+# clear modules cache if package is reloaded (after update?)
+prefix = __package__ + ".src"  # don't clear the base package
+for module_name in [
+    module_name
+    for module_name in sys.modules
+    if module_name.startswith(prefix)
+]:
+    del sys.modules[module_name]
+del prefix
+
+from .src import *  # noqa
 
 
 def plugin_loaded():
